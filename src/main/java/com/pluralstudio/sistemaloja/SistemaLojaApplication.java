e47692078300b8e938e59677de8ef8dc3e2d5381
allan.kleber <allan.kleber@esig.com.br>
2020-11-19 22:33:10 -0300

    primeiro commit

---
+++ src/main/java/com/pluralstudio/sistemaloja/SistemaLojaApplication.java
@@ -0,0 +1,13 @@
+package com.pluralstudio.sistemaloja;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaLojaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemaLojaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pluralstudio/sistemaloja/SistemaLojaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pluralstudio.sistemaloja;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaLojaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
