19d3933db6663f4bda794026e0d57299304ac44e
allan.kleber <allan.kleber@esig.com.br>
2020-11-29 18:42:21 -0300

    ItemPedido e ItemPedidoPk

--- src/main/java/com/pluralstudio/sistemaloja/SistemaLojaApplication.java
+++ src/main/java/com/pluralstudio/sistemaloja/SistemaLojaApplication.java
@@ -13,6 +13,7 @@ import com.pluralstudio.sistemaloja.domain.Cidade;
 import com.pluralstudio.sistemaloja.domain.Cliente;
 import com.pluralstudio.sistemaloja.domain.Endereco;
 import com.pluralstudio.sistemaloja.domain.Estado;
+import com.pluralstudio.sistemaloja.domain.ItemPedido;
 import com.pluralstudio.sistemaloja.domain.Pagamento;
 import com.pluralstudio.sistemaloja.domain.PagamentoComBoleto;
 import com.pluralstudio.sistemaloja.domain.PagamentoComCartao;
@@ -25,6 +26,7 @@ import com.pluralstudio.sistemaloja.repositories.CidadeRepository;
 import com.pluralstudio.sistemaloja.repositories.ClienteRepository;
 import com.pluralstudio.sistemaloja.repositories.EnderecoRepository;
 import com.pluralstudio.sistemaloja.repositories.EstadoRepository;
+import com.pluralstudio.sistemaloja.repositories.ItemPedidoRepository;
 import com.pluralstudio.sistemaloja.repositories.PagamentoRepository;
 import com.pluralstudio.sistemaloja.repositories.PedidoRepository;
 import com.pluralstudio.sistemaloja.repositories.ProdutoRepository;
@@ -55,6 +57,9 @@ public class SistemaLojaApplication implements CommandLineRunner{
 	
 	@Autowired
 	private PagamentoRepository pagamentoRepository;
+	
+	@Autowired
+	private ItemPedidoRepository itemPedidoRepository;
 	
 	public static void main(String[] args) {
 		SpringApplication.run(SistemaLojaApplication.class, args);
@@ -119,6 +124,21 @@ public class SistemaLojaApplication implements CommandLineRunner{
 		
 		pedidoRepository.saveAll(Arrays.asList(ped1,ped2));
 		pagamentoRepository.saveAll(Arrays.asList(pgto1,pgto2));
+		
+		ItemPedido ip1 = new ItemPedido(ped1, p1, 0.00, 1, 2000.00);
+		ItemPedido ip2 = new ItemPedido(ped1, p3, 0.00, 2, 80.00);
+		ItemPedido ip3 = new ItemPedido(ped2, p2, 100.00, 1, 800.00);
+		
+		ped1.getItens().addAll(Arrays.asList(ip1,ip2));
+		ped2.getItens().addAll(Arrays.asList(ip3));
+		
+		p1.getItens().addAll(Arrays.asList(ip1));
+		p2.getItens().addAll(Arrays.asList(ip3));
+		p3.getItens().addAll(Arrays.asList(ip2));
+		
+		itemPedidoRepository.saveAll(Arrays.asList(ip1,ip2,ip3));
+		
+		
 	}
 	
 	
--- src/main/java/com/pluralstudio/sistemaloja/domain/Produto.java
+++ src/main/java/com/pluralstudio/sistemaloja/domain/Produto.java
@@ -2,7 +2,9 @@ package com.pluralstudio.sistemaloja.domain;
 
 import java.io.Serializable;
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -11,6 +13,8 @@ import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.JoinTable;
 import javax.persistence.ManyToMany;
+import javax.persistence.OneToMany;
+
 
 import com.fasterxml.jackson.annotation.JsonBackReference;
 
@@ -35,6 +39,9 @@ public class Produto implements Serializable {
 	)
 	private List<Categoria> categorias = new ArrayList<>();
 	
+	@OneToMany(mappedBy ="id.produto" )
+	private Set<ItemPedido> itens = new HashSet<>();
+	
 	public Produto() {
 		
 	}
@@ -45,6 +52,15 @@ public class Produto implements Serializable {
 		this.nome = nome;
 		this.preco = preco;
 	}
+	
+	public List<Pedido> getPedidos(){
+		List<Pedido> lista = new ArrayList<>();
+		for (ItemPedido x : itens) {
+			lista.add(x.getPedido());
+		}
+		return lista;
+	}
+	
 
 	public Integer getId() {
 		return id;
@@ -68,6 +84,14 @@ public class Produto implements Serializable {
 
 	public void setPreco(Double preco) {
 		this.preco = preco;
+	}
+
+	public Set<ItemPedido> getItens() {
+		return itens;
+	}
+
+	public void setItens(Set<ItemPedido> itens) {
+		this.itens = itens;
 	}
 
 	public List<Categoria> getCategorias() {
