b9236dd6bf2a8e6c1d5b601945c16b099ffbfdfb
allan.kleber <allan.kleber@esig.com.br>
2020-12-01 20:45:03 -0300

    inserindo uma nova Categoria

--- src/main/java/com/pluralstudio/sistemaloja/resources/CategoriaResource.java
+++ src/main/java/com/pluralstudio/sistemaloja/resources/CategoriaResource.java
@@ -1,11 +1,15 @@
 package com.pluralstudio.sistemaloja.resources;
 
+import java.net.URI;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.pluralstudio.sistemaloja.domain.Categoria;
 import com.pluralstudio.sistemaloja.services.CategoriaService;
@@ -23,5 +27,14 @@ public class CategoriaResource {
 		return ResponseEntity.ok(obj);
 		
 		}
+	
+	@RequestMapping(method = RequestMethod.POST)
+	public ResponseEntity<Void> insert(@RequestBody	Categoria obj){
+		obj = service.insert(obj);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/id").
+				buildAndExpand(obj.getId()).toUri();
+		return ResponseEntity.created(uri).build();
+		
+	}
 
 }
