604498f2db0b9e81aba878bb00b2e2bcc7e5dfac
allan.kleber <allan.kleber@esig.com.br>
2020-12-01 21:49:54 -0300

    Listando todas as categorias com DTO

--- src/main/java/com/pluralstudio/sistemaloja/resources/CategoriaResource.java
+++ src/main/java/com/pluralstudio/sistemaloja/resources/CategoriaResource.java
@@ -1,6 +1,8 @@
 package com.pluralstudio.sistemaloja.resources;
 
 import java.net.URI;
+import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -12,6 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.pluralstudio.sistemaloja.domain.Categoria;
+import com.pluralstudio.sistemaloja.dto.CategoriaDTO;
 import com.pluralstudio.sistemaloja.services.CategoriaService;
 
 @RestController
@@ -49,5 +52,14 @@ public class CategoriaResource {
 		service.delete(id);
 		return ResponseEntity.noContent().build();
 	}
+	
+	@RequestMapping(method =RequestMethod.GET )
+	public ResponseEntity<List<CategoriaDTO>> findAll(){
+		List<Categoria> list=service.findAll();
+		List<CategoriaDTO> listDto = list.parallelStream().map(obj -> new CategoriaDTO(obj)).
+				collect(Collectors.toList());
+		return ResponseEntity.ok().body(listDto);
+		
+		}
 
 }
--- src/main/java/com/pluralstudio/sistemaloja/services/CategoriaService.java
+++ src/main/java/com/pluralstudio/sistemaloja/services/CategoriaService.java
@@ -1,5 +1,6 @@
 package com.pluralstudio.sistemaloja.services;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -44,6 +45,10 @@ public class CategoriaService {
 			
 		}
 		
+	}
+	
+	public List<Categoria> findAll(){
+		return repo.findAll();
 	}
 
 
