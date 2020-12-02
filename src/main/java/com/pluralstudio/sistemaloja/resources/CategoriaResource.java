aab5d5d6abf2015a6ad47f3d25d0fe1e12042393
allan.kleber <allan.kleber@esig.com.br>
2020-12-01 21:04:53 -0300

    Atualizando categorias com put, outros pequenos ajustes

--- src/main/java/com/pluralstudio/sistemaloja/resources/CategoriaResource.java
+++ src/main/java/com/pluralstudio/sistemaloja/resources/CategoriaResource.java
@@ -22,7 +22,7 @@ public class CategoriaResource {
 	private CategoriaService service;
 	
 	@RequestMapping(value = "/{id}",method =RequestMethod.GET )
-	public ResponseEntity<?> find(@PathVariable Integer id){
+	public ResponseEntity<Categoria> find(@PathVariable Integer id){
 		Categoria obj=service.find(id);
 		return ResponseEntity.ok(obj);
 		
@@ -35,6 +35,13 @@ public class CategoriaResource {
 				buildAndExpand(obj.getId()).toUri();
 		return ResponseEntity.created(uri).build();
 		
+	}
+	
+	@RequestMapping(value = "/{id}",method =RequestMethod.PUT)
+	public ResponseEntity<Void> update(@RequestBody	Categoria obj,@PathVariable Integer id){
+		obj.setId(id);
+		obj = service.update(obj);
+		return ResponseEntity.noContent().build();
 	}
 
 }
