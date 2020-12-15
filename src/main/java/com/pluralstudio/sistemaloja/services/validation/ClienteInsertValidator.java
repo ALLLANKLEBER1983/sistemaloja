6be2c37b0e455f04a15940ea0f0a1f6d19a86cbf
allan.kleber <allan.kleber@esig.com.br>
2020-12-14 21:27:24 -0300

    Validação customizada de cnpj e cpf

---
+++ src/main/java/com/pluralstudio/sistemaloja/services/validation/ClienteInsertValidator.java
@@ -0,0 +1,39 @@
+package com.pluralstudio.sistemaloja.services.validation;
+
+import java.util.ArrayList;
+import java.util.List;
+import javax.validation.ConstraintValidator;
+import javax.validation.ConstraintValidatorContext;
+
+import com.pluralstudio.sistemaloja.domain.enums.TipoCliente;
+import com.pluralstudio.sistemaloja.dto.ClienteNewDTO;
+import com.pluralstudio.sistemaloja.resources.exceptions.FieldMessage;
+import com.pluralstudio.sistemaloja.services.validation.utils.BR;
+
+public class ClienteInsertValidator implements ConstraintValidator<ClienteInsert, ClienteNewDTO> {
+	@Override
+	public void initialize(ClienteInsert ann) {
+	}
+
+	@Override
+	public boolean isValid(ClienteNewDTO objDto, ConstraintValidatorContext context) {
+		List<FieldMessage> list = new ArrayList<>();
+		
+		if(objDto.getTipo().equals(TipoCliente.PESSOAFISICA.getCod()) &&
+				!BR.isValidCpf(objDto.getCpfOuCnpj())) {
+			list.add(new FieldMessage("cpfOuCnpj","CPF inválido"));
+		}
+		
+		if(objDto.getTipo().equals(TipoCliente.PESSOAJURIDICA.getCod()) &&
+				!BR.isValidCnpj(objDto.getCpfOuCnpj())) {
+			list.add(new FieldMessage("cpfOuCnpj","CNPJ inválido"));
+		}
+		// inclua os testes aqui, inserindo erros na lista
+		for (FieldMessage e : list) {
+			context.disableDefaultConstraintViolation();
+			context.buildConstraintViolationWithTemplate(e.getMessage()).addPropertyNode(e.getFieldName())
+					.addConstraintViolation();
+		}
+		return list.isEmpty();
+	}
+}
