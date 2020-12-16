5d02cea520c7894f2ce36cafe8a7d60310df3878
allan.kleber <allan.kleber@esig.com.br>
2020-12-15 21:27:59 -0300

    Validação customizada: email não repetido na atualização do cliente

--- src/main/java/com/pluralstudio/sistemaloja/dto/ClienteDTO.java
+++ src/main/java/com/pluralstudio/sistemaloja/dto/ClienteDTO.java
@@ -7,7 +7,8 @@ import org.hibernate.validator.constraints.Length;
 import org.hibernate.validator.constraints.NotEmpty;
 
 import com.pluralstudio.sistemaloja.domain.Cliente;
-
+import com.pluralstudio.sistemaloja.services.validation.ClienteUpdate;
+@ClienteUpdate
 public class ClienteDTO implements Serializable {
 
 	private static final long serialVersionUID = 1L;
