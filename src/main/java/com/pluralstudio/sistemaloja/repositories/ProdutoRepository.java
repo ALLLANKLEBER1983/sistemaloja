e5b08766b01d17ec7f42186168c6b68c9850dd85
allan.kleber <allan.kleber@esig.com.br>
2020-12-17 22:06:45 -0300

    Busca de produtos por nome e categoria

--- src/main/java/com/pluralstudio/sistemaloja/repositories/ProdutoRepository.java
+++ src/main/java/com/pluralstudio/sistemaloja/repositories/ProdutoRepository.java
@@ -1,13 +1,28 @@
 package com.pluralstudio.sistemaloja.repositories;
 
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
+
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.pluralstudio.sistemaloja.domain.Categoria;
 import com.pluralstudio.sistemaloja.domain.Produto;
 
 
 
 @Repository
 public interface ProdutoRepository extends JpaRepository<Produto,Integer>{
+	
+	@Transactional(readOnly=true)
+	@Query("SELECT DISTINCT obj From Produto obj INNER JOIN obj.categorias cat WHERE obj.nome LIKE %:nome% AND cat IN :categorias")
+	Page<Produto> seach(@Param("nome") String nome,@Param("categorias")List<Categoria> categorias,Pageable pageRequest);
 
 }
