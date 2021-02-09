44258f985a486f81b0902c7c09a502178dc7cb9b
allan.kleber <allan.kleber@esig.com.br>
2021-02-09 20:14:52 -0300

    Implementando toString de pedido

--- src/main/java/com/pluralstudio/sistemaloja/services/PedidoService.java
+++ src/main/java/com/pluralstudio/sistemaloja/services/PedidoService.java
@@ -15,6 +15,7 @@ import com.pluralstudio.sistemaloja.domain.enums.EstadoPagamento;
 import com.pluralstudio.sistemaloja.repositories.ItemPedidoRepository;
 import com.pluralstudio.sistemaloja.repositories.PagamentoRepository;
 import com.pluralstudio.sistemaloja.repositories.PedidoRepository;
+import com.pluralstudio.sistemaloja.repositories.ProdutoRepository;
 import com.pluralstudio.sistemaloja.services.exception.ObjectNotFoundException;
 
 @Service
@@ -35,6 +36,9 @@ public class PedidoService {
 	@Autowired
 	private ItemPedidoRepository itemPedidoRepository;
 	
+	@Autowired
+	private ClienteService clienteService;
+	
 	public Pedido find(Integer id) {
 		 Optional<Pedido> obj = repo.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException(
@@ -45,23 +49,24 @@ public class PedidoService {
 	public Pedido insert(Pedido obj) {
 		obj.setId(null);
 		obj.setInstante(new Date());
+		obj.setCliente(clienteService.find(obj.getCliente().getId()));
 		obj.getPagamento().setEstado(EstadoPagamento.PENDENTE);
 		obj.getPagamento().setPedido(obj);
-		if(obj.getPagamento() instanceof PagamentoComBoleto) {
+		if (obj.getPagamento() instanceof PagamentoComBoleto) {
 			PagamentoComBoleto pagto = (PagamentoComBoleto) obj.getPagamento();
-			boletoService.preencherPagamentoComBoleto(pagto,obj.getInstante());
+			boletoService.preencherPagamentoComBoleto(pagto, obj.getInstante());
 		}
 		obj = repo.save(obj);
 		pagamentoRepository.save(obj.getPagamento());
-		for(ItemPedido ip :obj.getItens()) {
+		for (ItemPedido ip : obj.getItens()) {
 			ip.setDesconto(0.0);
 			ip.setProduto(produtoService.find(ip.getProduto().getId()));
 			ip.setPreco(ip.getProduto().getPreco());
 			ip.setPedido(obj);
 		}
 		itemPedidoRepository.saveAll(obj.getItens());
+		System.out.println(obj);
 		return obj;
-		
 	}
 
 
